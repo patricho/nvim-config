48c89780a52dee93e962ff1fca9b22af1075bb39
Patrik Bauhn <patrik@bauhn.se>
2025-06-23 17:12:35 +0200

    Nvim Surround

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,6 +1,17 @@
 ---@type LazySpec
 return {
     {
+        "kylechui/nvim-surround",
+        version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+
+    {
         "lukas-reineke/virt-column.nvim",
         opts = {
             char = "▕" ,
