49e9ff6483eda5358af62299be9dfe70582345a8
Patrik Bauhn <patrik@bauhn.se>
2025-07-29 11:35:30 +0200

    Avante models

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -52,24 +52,28 @@ return {
         version = false,
         opts = {
             provider = "claude",
-            claude = {
-                endpoint = "https://api.anthropic.com",
-                model = "claude-3-7-sonnet-20250219",
-                temperature = 0,
-                max_tokens = 8192,
+            providers = {
+                claude = {
+                    endpoint = "https://api.anthropic.com",
+                    model = "claude-sonnet-4-20250514",
+                    extra_request_body = {
+                        temperature = 0,
+                        max_tokens = 8192,
+                    },
+                },
+                gemini = {
+                    endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+                    model = "gemini-2.5-flash",
+                    timeout = 30000,
+                    temperature = 0,
+                    max_tokens = 8192,
+                    api_key_name = "GEMINI_API_KEY"
+                },
+                ollama = {
+                    model = "qwen2.5-coder:latest",
+                },
             },
-            gemini = {
-                endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-                model = "gemini-2.5-flash-preview-04-17",
-                timeout = 30000,
-                temperature = 0,
-                max_tokens = 8192,
-                api_key_name = "GEMINI_API_KEY"
-            },
-            ollama = {
-                model = "qwen2.5-coder:latest",
-            },
-            cursor_applying_provider = "ollama",
+            cursor_applying_provider = "gemini",
             behaviour = {
                 enable_cursor_planning_mode = true,
             },
