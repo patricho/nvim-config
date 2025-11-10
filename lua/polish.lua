8d178d88a9503b48f9ea5086ceef6245a31fd960
Patrik Bauhn <patrik@bauhn.se>
2025-11-10 22:36:58 +0100

    Yank path keymaps

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,51 +1,79 @@
 -- TODO: move to astrocore.lua
-vim.keymap.set('n', '§', 'yiw', { desc = 'Yank inner word' })
-vim.keymap.set('n', '°', '"_diwP', { desc = 'Paste inner word' })
-vim.keymap.set('n', '½', '"_diwP', { desc = 'Paste inner word' })
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Show hover information' })
-vim.keymap.set('n', 'gh', vim.lsp.buf.hover, { desc = 'Show hover information' })
-vim.keymap.set('n', '<leader>bp', '<cmd>BufferPrevious<cr>', { desc = 'Previous buffer' })
-vim.keymap.set('n', '<leader>bn', '<cmd>BufferNext<cr>', { desc = 'Previous buffer' })
-vim.keymap.set('n', '<tab>', '<cmd>BufferNext<cr>', { desc = 'Previous buffer' })
-vim.keymap.set('n', '<S-tab>', '<cmd>BufferPrevious<cr>', { desc = 'Previous buffer' })
-vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, { desc = 'Show hover information' })
-vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { desc = 'Rename symbol under cursor' })
-vim.keymap.set('n', '<leader>fs', require("telescope.builtin").lsp_document_symbols, { desc = 'Search file symbols' })
-vim.keymap.set('n', '<leader>fS', vim.lsp.buf.workspace_symbol, { desc = 'Search workspace symbols' })
-vim.keymap.set('n', '<C-S-i>', '<cmd>cnext<cr>', { desc = 'Next quickfix item' })
-vim.keymap.set('n', '<M-S-i>', '<cmd>cnext<cr>', { desc = 'Next quickfix item' })
-vim.keymap.set('n', '<C-S-o>', '<cmd>cprev<cr>', { desc = 'Previous quickfix item' })
-vim.keymap.set('n', '<M-S-o>', '<cmd>cprev<cr>', { desc = 'Previous quickfix item' })
-vim.keymap.set('n', '<M-S-o>', '<cmd>cprev<cr>', { desc = 'Previous quickfix item' })
-vim.keymap.set('n', '<Leader>mm', require('recall').toggle, { desc = 'Recall toggle' })
-vim.keymap.set('n', 'mm'        , require('recall').toggle, { desc = 'Recall toggle' })
-vim.keymap.set('n', '<Leader>mn', require('recall').goto_next, { desc = 'Recall next' })
-vim.keymap.set('n', 'mn'        , require('recall').goto_next, { desc = 'Recall next' })
-vim.keymap.set('n', '<C-m>'     , require('recall').goto_next, { desc = 'Recall next' })
-vim.keymap.set('n', '<Leader>mp', require('recall').goto_prev, { desc = 'Recall previous' })
-vim.keymap.set('n', 'mp'        , require('recall').goto_prev, { desc = 'Recall previous' })
-vim.keymap.set('n', '<Leader>mc', require('recall').clear, { desc = 'Recall clear' })
-vim.keymap.set('n', 'mc'        , require('recall').clear, { desc = 'Recall clear' })
-vim.keymap.del('n', '<Leader>q') -- Don't quit window
-vim.keymap.del('n', '<leader>o') -- Remove bindings that conflicts with OpenCode, that's not used anyway
+vim.keymap.set("n", "§", "yiw", { desc = "Yank inner word" })
+vim.keymap.set("n", "°", '"_diwP', { desc = "Paste inner word" })
+vim.keymap.set("n", "½", '"_diwP', { desc = "Paste inner word" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover information" })
+vim.keymap.set("n", "gh", vim.lsp.buf.hover, { desc = "Show hover information" })
+vim.keymap.set("n", "<leader>bp", "<cmd>BufferPrevious<cr>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>bn", "<cmd>BufferNext<cr>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<tab>", "<cmd>BufferNext<cr>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<S-tab>", "<cmd>BufferPrevious<cr>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, { desc = "Show hover information" })
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol under cursor" })
+vim.keymap.set("n", "<leader>fs", require("telescope.builtin").lsp_document_symbols, { desc = "Search file symbols" })
+vim.keymap.set("n", "<leader>fS", vim.lsp.buf.workspace_symbol, { desc = "Search workspace symbols" })
+vim.keymap.set("n", "<C-S-i>", "<cmd>cnext<cr>", { desc = "Next quickfix item" })
+vim.keymap.set("n", "<M-S-i>", "<cmd>cnext<cr>", { desc = "Next quickfix item" })
+vim.keymap.set("n", "<C-S-o>", "<cmd>cprev<cr>", { desc = "Previous quickfix item" })
+vim.keymap.set("n", "<M-S-o>", "<cmd>cprev<cr>", { desc = "Previous quickfix item" })
+vim.keymap.set("n", "<M-S-o>", "<cmd>cprev<cr>", { desc = "Previous quickfix item" })
+vim.keymap.set("n", "<Leader>mm", require("recall").toggle, { desc = "Recall toggle" })
+vim.keymap.set("n", "mm", require("recall").toggle, { desc = "Recall toggle" })
+vim.keymap.set("n", "<Leader>mn", require("recall").goto_next, { desc = "Recall next" })
+vim.keymap.set("n", "mn", require("recall").goto_next, { desc = "Recall next" })
+vim.keymap.set("n", "<C-m>", require("recall").goto_next, { desc = "Recall next" })
+vim.keymap.set("n", "<Leader>mp", require("recall").goto_prev, { desc = "Recall previous" })
+vim.keymap.set("n", "mp", require("recall").goto_prev, { desc = "Recall previous" })
+vim.keymap.set("n", "<Leader>mc", require("recall").clear, { desc = "Recall clear" })
+vim.keymap.set("n", "mc", require("recall").clear, { desc = "Recall clear" })
+vim.keymap.del("n", "<Leader>q") -- Don't quit window
+vim.keymap.del("n", "<leader>o") -- Remove bindings that conflicts with OpenCode, that's not used anyway
+
+vim.keymap.set("n", "<Leader>ypr", "<cmd>:let @+=expand('%:.')<cr>", { desc = "Yank buffer's relative path" })
+vim.keymap.set("n", "<Leader>ypa", "<cmd>:let @+=expand('%:p')<cr>", { desc = "Yank buffer's absolute path" })
+vim.keymap.set("v", "<Leader>ypr", function()
+  local relative_path = vim.fn.expand "%"
+  local start_line = vim.fn.line "v"
+  local end_line = vim.fn.line "."
+  -- Ensure start is before end
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+  local result = relative_path .. ":" .. start_line .. "-" .. end_line
+  vim.fn.setreg("+", result)
+  vim.notify("Yanked: " .. result)
+end, { desc = "Yank buffer's relative path with line range" })
+
+vim.keymap.set("v", "<leader>ypa", function()
+  local absolute_path = vim.fn.expand "%:p"
+  local start_line = vim.fn.line "v"
+  local end_line = vim.fn.line "."
+  -- Ensure start is before end
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+  local result = absolute_path .. ":" .. start_line .. "-" .. end_line
+  vim.fn.setreg("+", result)
+  vim.notify("Yanked: " .. result)
+end, { desc = "Yank buffer's absolute path with line range" })
 
 vim.o.updatetime = 400
 vim.o.autoread = true
 vim.o.scrolloff = 8
-vim.cmd("packadd cfilter")
+vim.cmd "packadd cfilter"
 
 -- Color column
 vim.api.nvim_set_hl(0, "VirtColumn", { fg = "#383230" })
 
 -- So gq and gw can be used to properly wrap comments
-vim.cmd("set formatexpr=")
-vim.cmd("set textwidth=100")
+vim.cmd "set formatexpr="
+vim.cmd "set textwidth=100"
 
 -- Cursor colors and behavior
-vim.cmd("highlight Cursor guibg=#ffcf88 ctermbg=67")
-vim.cmd("highlight iCursor guibg=#ffff00 ctermbg=229")
-vim.cmd("highlight rCursor guibg=#d73333 ctermbg=124")
-vim.cmd("set guicursor=n-v-c:block-Cursor/lCursor,i-ci-ve:ver100-iCursor,r-cr:block-rCursor,o:hor50-Cursor/lCursor,sm:block-iCursor,i:blinkwait1000-blinkon500-blinkoff250")
+vim.cmd "highlight Cursor guibg=#ffcf88 ctermbg=67"
+vim.cmd "highlight iCursor guibg=#ffff00 ctermbg=229"
+vim.cmd "highlight rCursor guibg=#d73333 ctermbg=124"
+vim.cmd "set guicursor=n-v-c:block-Cursor/lCursor,i-ci-ve:ver100-iCursor,r-cr:block-rCursor,o:hor50-Cursor/lCursor,sm:block-iCursor,i:blinkwait1000-blinkon500-blinkoff250"
 
 -- Diagnostic colors
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { fg = "#db4b4b", sp = "#ff0000", underline = true })
@@ -62,67 +90,67 @@ vim.api.nvim_set_hl(0, "HeirlineInsert", { fg = "#000000", bg = "#a6e3a1" })
 vim.api.nvim_set_hl(0, "HeirlineVisual", { fg = "#000000", bg = "#fe8019" })
 vim.api.nvim_set_hl(0, "HeirlineReplace", { fg = "#ffffff", bg = "#d73333" })
 
-vim.api.nvim_set_hl(0, "YankHighlight", { fg = "#fe8019", bg ="#500f08" })
-vim.api.nvim_set_hl(0, "LocalHighlight", { bg ="#702020" })
-vim.api.nvim_set_hl(0, "NotifyBackground", { bg ="#000000" })
+vim.api.nvim_set_hl(0, "YankHighlight", { fg = "#fe8019", bg = "#500f08" })
+vim.api.nvim_set_hl(0, "LocalHighlight", { bg = "#702020" })
+vim.api.nvim_set_hl(0, "NotifyBackground", { bg = "#000000" })
 
 -- Search colors
-vim.api.nvim_set_hl(0, "Search", { fg = '#d73333' })
-vim.api.nvim_set_hl(0, "CurSearch", { fg = '#d73333', bold = true })
-vim.api.nvim_set_hl(0, "HlSearchNear", { fg = '#d73333', bold = true })
+vim.api.nvim_set_hl(0, "Search", { fg = "#d73333" })
+vim.api.nvim_set_hl(0, "CurSearch", { fg = "#d73333", bold = true })
+vim.api.nvim_set_hl(0, "HlSearchNear", { fg = "#d73333", bold = true })
 vim.api.nvim_set_hl(0, "HlSearchLens", { fg = "#777777" })
 vim.api.nvim_set_hl(0, "HlSearchLensNear", { fg = "#aaaaaa" })
 
 -- Current symbol LSP highlight colors
-vim.api.nvim_set_hl(0, 'LspReferenceText',     { fg = '#fe8019' })
-vim.api.nvim_set_hl(0, 'LspReferenceRead',     { fg = '#fe8019' })
-vim.api.nvim_set_hl(0, 'LspReferenceWrite',    { fg = '#fe8019', bold = true, underline = true })
-vim.api.nvim_set_hl(0, 'IlluminatedWordText',  { fg = '#fe8019' })
-vim.api.nvim_set_hl(0, 'IlluminatedWordRead',  { fg = '#fe8019' })
-vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { fg = '#fe8019', bold = true, underline = true })
+vim.api.nvim_set_hl(0, "LspReferenceText", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "LspReferenceRead", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "LspReferenceWrite", { fg = "#fe8019", bold = true, underline = true })
+vim.api.nvim_set_hl(0, "IlluminatedWordText", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "IlluminatedWordRead", { fg = "#fe8019" })
+vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { fg = "#fe8019", bold = true, underline = true })
 
 -- Git diff styling
-vim.opt.fillchars = { diff = '╱' }
-vim.api.nvim_set_hl(0, 'DiffAdd',                  { bg = '#0f4412' })
-vim.api.nvim_set_hl(0, 'GitSignsAdd',              { fg = '#1d982e' })
+vim.opt.fillchars = { diff = "╱" }
+vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#0f4412" })
+vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#1d982e" })
 -- vim.api.nvim_set_hl(0, 'ScrollbarGitAdd',       { fg = '#1d982e' })
 -- vim.api.nvim_set_hl(0, 'ScrollbarGitAddHandle', { fg = '#1d982e', bg = '#444444' })
-vim.api.nvim_set_hl(0, 'DiffDelete',               { fg = '#962623' })
-vim.api.nvim_set_hl(0, 'GitSignsDelete',           { fg = '#962623' })
+vim.api.nvim_set_hl(0, "DiffDelete", { fg = "#962623" })
+vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = "#962623" })
 -- vim.api.nvim_set_hl(0, 'ScrollbarGitDelete',    { fg = '#962623' })
 -- vim.api.nvim_set_hl(0, 'ScrollbarGitDeleteHandle', { fg = '#962623', bg = '#444444' })
-vim.api.nvim_set_hl(0, 'DiffText',                 { bg = '#466570' })
-vim.api.nvim_set_hl(0, 'DiffChange',               { bg = '#25343c' })
-vim.api.nvim_set_hl(0, 'GitSignsChange',           { fg = '#466570' })
+vim.api.nvim_set_hl(0, "DiffText", { bg = "#466570" })
+vim.api.nvim_set_hl(0, "DiffChange", { bg = "#25343c" })
+vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#466570" })
 -- vim.api.nvim_set_hl(0, 'ScrollbarGitChange',       { fg = '#466570' })
 -- vim.api.nvim_set_hl(0, 'ScrollbarGitChangeHandle', { fg = '#466570', bg = '#444444' })
 
 -- NeoTree files
-vim.api.nvim_set_hl(0, 'NeoTreeGitAdded',     { fg = '#5db85e' })
-vim.api.nvim_set_hl(0, 'NeoTreeGitStaged',    { fg = '#5db85e' })
-vim.api.nvim_set_hl(0, 'NeoTreeGitDeleted',   { fg = '#962623' })
-vim.api.nvim_set_hl(0, 'NeoTreeGitConflict',  { fg = '#962623' })
-vim.api.nvim_set_hl(0, 'NeoTreeGitRenamed',   { fg = '#ed965d' })
-vim.api.nvim_set_hl(0, 'NeoTreeGitModified',  { fg = '#ed965d' })
-vim.api.nvim_set_hl(0, 'NeoTreeGitUnstaged',  { fg = '#b6b5c0' })
-vim.api.nvim_set_hl(0, 'NeoTreeGitUntracked', { fg = '#b6b5c0' })
+vim.api.nvim_set_hl(0, "NeoTreeGitAdded", { fg = "#5db85e" })
+vim.api.nvim_set_hl(0, "NeoTreeGitStaged", { fg = "#5db85e" })
+vim.api.nvim_set_hl(0, "NeoTreeGitDeleted", { fg = "#962623" })
+vim.api.nvim_set_hl(0, "NeoTreeGitConflict", { fg = "#962623" })
+vim.api.nvim_set_hl(0, "NeoTreeGitRenamed", { fg = "#ed965d" })
+vim.api.nvim_set_hl(0, "NeoTreeGitModified", { fg = "#ed965d" })
+vim.api.nvim_set_hl(0, "NeoTreeGitUnstaged", { fg = "#b6b5c0" })
+vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { fg = "#b6b5c0" })
 
 -- Barbar tabline
-vim.api.nvim_set_hl(0, 'BufferCurrent', { fg = '#bbbbbb', bg = '#282828', bold = true })
-vim.api.nvim_set_hl(0, 'BufferCurrentMod', { fg = '#bbbbbb', bg = '#282828', bold = true })
-vim.api.nvim_set_hl(0, 'BufferCurrentBtn', { fg = '#555555', bg = '#282828' })
-vim.api.nvim_set_hl(0, 'BufferCurrentADDED', { fg = '#238636', bg = '#282828' })
-vim.api.nvim_set_hl(0, 'BufferCurrentCHANGED', { fg = '#bd561d', bg = '#282828' })
-vim.api.nvim_set_hl(0, 'BufferCurrentDELETED', { fg = '#da3633', bg = '#282828' })
-vim.api.nvim_set_hl(0, 'BufferCurrentERROR', { fg = '#d73333', bg = '#282828' })
-vim.api.nvim_set_hl(0, 'BufferCurrentSign', { fg = '#282828', bg = '#282828' })
-vim.api.nvim_set_hl(0, 'BufferInactiveSign', { fg = '#282828', bg = '#1f1a18' })
-vim.api.nvim_set_hl(0, 'BufferInactive', { fg = '#666666', bg = '#1f1a18' })
-vim.api.nvim_set_hl(0, 'BufferInactiveMod', { fg = '#666666', bg = '#1f1a18' })
-vim.api.nvim_set_hl(0, 'BufferInactiveBtn', { fg = '#333333', bg = '#1f1a18' })
-vim.api.nvim_set_hl(0, 'BufferTabpagesSep', { fg = '#00ff00', bg = '#00ff00' })
-vim.api.nvim_set_hl(0, 'BufferTabpageFill', { bg = '#1f1a18' })
-vim.api.nvim_set_hl(0, 'BufferTabpages', { fg = '#00ff00', bg = '#00ff00' })
+vim.api.nvim_set_hl(0, "BufferCurrent", { fg = "#bbbbbb", bg = "#282828", bold = true })
+vim.api.nvim_set_hl(0, "BufferCurrentMod", { fg = "#bbbbbb", bg = "#282828", bold = true })
+vim.api.nvim_set_hl(0, "BufferCurrentBtn", { fg = "#555555", bg = "#282828" })
+vim.api.nvim_set_hl(0, "BufferCurrentADDED", { fg = "#238636", bg = "#282828" })
+vim.api.nvim_set_hl(0, "BufferCurrentCHANGED", { fg = "#bd561d", bg = "#282828" })
+vim.api.nvim_set_hl(0, "BufferCurrentDELETED", { fg = "#da3633", bg = "#282828" })
+vim.api.nvim_set_hl(0, "BufferCurrentERROR", { fg = "#d73333", bg = "#282828" })
+vim.api.nvim_set_hl(0, "BufferCurrentSign", { fg = "#282828", bg = "#282828" })
+vim.api.nvim_set_hl(0, "BufferInactiveSign", { fg = "#282828", bg = "#1f1a18" })
+vim.api.nvim_set_hl(0, "BufferInactive", { fg = "#666666", bg = "#1f1a18" })
+vim.api.nvim_set_hl(0, "BufferInactiveMod", { fg = "#666666", bg = "#1f1a18" })
+vim.api.nvim_set_hl(0, "BufferInactiveBtn", { fg = "#333333", bg = "#1f1a18" })
+vim.api.nvim_set_hl(0, "BufferTabpagesSep", { fg = "#00ff00", bg = "#00ff00" })
+vim.api.nvim_set_hl(0, "BufferTabpageFill", { bg = "#1f1a18" })
+vim.api.nvim_set_hl(0, "BufferTabpages", { fg = "#00ff00", bg = "#00ff00" })
 
 -- Telescope picker
 -- vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { fg = "#ffffff" })
